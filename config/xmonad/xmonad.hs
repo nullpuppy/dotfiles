50afffa05307a16700f45018e85f1158c5ac65c3
Dustin Knie <dustin@nulldomain.com>
2014-02-18 08:22:36 -0800

    Use gnome-terminal instead of urxvt

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -35,7 +35,7 @@ xF86AudioNext = 0x1008FF17
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "urxvt"
+myTerminal      = "gnome-terminal"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
